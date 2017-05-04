9692d9ff91cb4ab1f1f8c6ff238ed8f564aec9d4
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-05-04 12:43:49 +0200

    Added first yaml schema for catalog manager and swagger ui updated

---
+++ catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
@@ -0,0 +1,58 @@
+
+import play.api.mvc.{Action,Controller}
+
+import play.api.data.validation.Constraint
+
+import play.api.i18n.MessagesApi
+
+import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
+
+import de.zalando.play.controllers._
+
+import PlayBodyParsing._
+
+import PlayValidations._
+
+import scala.util._
+
+import javax.inject._
+
+
+/**
+ * This controller is re-generated after each change in the specification.
+ * Please only place your hand-written code between appropriate comments in the body of the controller.
+ */
+
+package catalog_manager.yaml {
+    // ----- Start of unmanaged code area for package Catalog_managerYaml
+                                                                                                                                                                                                                                                                                                                                                                                        
+    // ----- End of unmanaged code area for package Catalog_managerYaml
+    class Catalog_managerYaml @Inject() (
+        // ----- Start of unmanaged code area for injections Catalog_managerYaml
+
+        // ----- End of unmanaged code area for injections Catalog_managerYaml
+        val messagesApi: MessagesApi,
+        lifecycle: ApplicationLifecycle,
+        config: ConfigurationProvider
+    ) extends Catalog_managerYamlBase {
+        // ----- Start of unmanaged code area for constructor Catalog_managerYaml
+
+        // ----- End of unmanaged code area for constructor Catalog_managerYaml
+        val datasetcatalogs = datasetcatalogsAction {  _ =>  
+            // ----- Start of unmanaged code area for action  Catalog_managerYaml.datasetcatalogs
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  Catalog_managerYaml.datasetcatalogs
+        }
+        val datasetcatalogbyid = datasetcatalogbyidAction { (catalogid: String) =>  
+            // ----- Start of unmanaged code area for action  Catalog_managerYaml.datasetcatalogbyid
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  Catalog_managerYaml.datasetcatalogbyid
+        }
+        val createdatasetcatalog = createdatasetcatalogAction { (catalog: MetaCatalog) =>  
+            // ----- Start of unmanaged code area for action  Catalog_managerYaml.createdatasetcatalog
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  Catalog_managerYaml.createdatasetcatalog
+        }
+    
+    }
+}

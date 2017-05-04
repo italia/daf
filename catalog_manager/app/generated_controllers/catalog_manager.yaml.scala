cbbf7b08fbca54b94be1b34e610cfdd3a28a303f
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-05-04 16:29:46 +0200

    Conform to api principles design

--- catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
+++ catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
@@ -25,7 +25,7 @@ import javax.inject._
 
 package catalog_manager.yaml {
     // ----- Start of unmanaged code area for package Catalog_managerYaml
-                                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                                            
     // ----- End of unmanaged code area for package Catalog_managerYaml
     class Catalog_managerYaml @Inject() (
         // ----- Start of unmanaged code area for injections Catalog_managerYaml

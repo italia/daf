7c9d5a458ab75f7b4a44034eb1f74d09eac31be9
David Greco <greco@acm.org>
2017-08-03 12:05:44 +0200

    [DAF-81] - Expose kong on a public address via https

--- certbot/scripts/create_new_certificate.sh
+++ certbot/scripts/create_new_certificate.sh
@@ -4,13 +4,13 @@ kubectl delete -f ./../../ingress/nginx-ingress-controller.yml
 
 ssh root@edge1  \
     docker run -v /glusterfs/volume1/certbot/conf/:/etc/letsencrypt/ --network host -t 10.98.74.120:5000/daf-certbot:1.0.0 \
-    certbot certonly --standalone --preferred-challenges tls-sni  --register-unsafely-without-email --agree-tos -d daf-superset.teamdigitale.governo.it
+    certbot certonly --standalone --preferred-challenges tls-sni  --register-unsafely-without-email --agree-tos -d daf.teamdigitale.governo.it,daf-superset.teamdigitale.governo.it
 
-kubectl delete secret tls-secret
+kubectl delete secret tls-daf-secret
 
 ssh root@edge1 \
-    kubectl create secret tls tls-secret \
-        --cert=/glusterfs/volume1/certbot/conf/live/daf-superset.teamdigitale.governo.it/fullchain.pem \
-        --key=/glusterfs/volume1/certbot/conf/live/daf-superset.teamdigitale.governo.it/privkey.pem
+    kubectl create secret tls tls-daf-secret \
+        --cert=/glusterfs/volume1/certbot/conf/live/daf.teamdigitale.governo.it/fullchain.pem \
+        --key=/glusterfs/volume1/certbot/conf/live/daf.teamdigitale.governo.it/privkey.pem
 
 kubectl create -f ./../../ingress/nginx-ingress-controller.yml
--- kong/management/add_apis.sh
+++ kong/management/add_apis.sh
@@ -3,47 +3,23 @@
 curl -i -X POST \
   --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
   --data 'name=daf-storage-manager' \
-  --data 'uris=/storage-manager/v1' \
-  --data 'upstream_url=http://storage-manager.default.svc.cluster.local:9000/storage-manager/v1'
+  --data 'uris=/storage-manager' \
+  --data 'upstream_url=http://storage-manager.default.svc.cluster.local:9000/storage-manager'
 
 curl -i -X POST \
   --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
   --data 'name=daf-security-manager' \
-  --data 'uris=/security-manager/v1' \
-  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/security-manager/v1'
-
-curl -i -X POST \
-  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
-  --data 'name=swagger-ui-docs' \
-  --data 'uris=/docs/swagger-ui' \
-  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/docs/swagger-ui'
-
-curl -i -X POST \
-  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
-  --data 'name=daf-security-manager-swagger' \
-  --data 'uris=/security-manager/swagger-ui/index.html' \
-  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/swagger-ui/index.html'
-
-curl -i -X POST \
-  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
-  --data 'name=daf-storage-manager-swagger' \
-  --data 'uris=/storage-manager/swagger-ui/index.html' \
-  --data 'upstream_url=http://storage-manager.default.svc.cluster.local:9000/swagger-ui/index.html'
-
-curl -i -X POST \
-  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
-  --data 'name=daf-security-manager-swagger-json' \
-  --data 'uris=/security-manager/swagger.json' \
-  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/security-manager/swagger.json'
-
-curl -i -X POST \
-  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
-  --data 'name=daf-storage-manager-swagger-json' \
-  --data 'uris=/storage-manager/swagger.json' \
-  --data 'upstream_url=http://storage-manager.default.svc.cluster.local:9000/storage-manager/swagger.json'
+  --data 'uris=/security-manager' \
+  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/security-manager'
 
 curl -i -X POST \
   --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
   --data 'name=daf-opentsdb' \
   --data 'uris=/opentsdb/v1' \
   --data 'upstream_url=http://opentsdb.default.svc.cluster.local:4242'
+
+curl -i -X POST \
+  --url http://kong-admin.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-livy' \
+  --data 'uris=/livy' \
+  --data 'upstream_url=http://livy.default.svc.cluster.local:8998'

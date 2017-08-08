e5b7aa672e4faedb1eaa10897bcebf3d7146ed5d
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-08-08 22:43:43 +0200

    [DATAPOR-33] Docker Ckan integration on cluster

--- catalog_manager/app/it/gov/daf/catalogmanager/utilities/AppConfig.scala
+++ catalog_manager/app/it/gov/daf/catalogmanager/utilities/AppConfig.scala
@@ -15,7 +15,11 @@ private class AppConfig @Inject()(playConfig: Configuration) {
   val ckanHost: Option[String] = playConfig.getString("app.ckan.url")
   val dbHost: Option[String] = playConfig.getString("mongo.host")
   val dbPort: Option[Int] = playConfig.getInt("mongo.port")
+  val userName :Option[String] = playConfig.getString("mongo.username")
+  val password :Option[String] = playConfig.getString("mongo.password")
+  val database :Option[String] = playConfig.getString("mongo.database")
 }
+
 
 
 object ConfigReader {
@@ -23,5 +27,8 @@ object ConfigReader {
   def getCkanHost: String = config.ckanHost.getOrElse("localhost")
   def getDbHost: String = config.dbHost.getOrElse("localhost")
   def getDbPort: Int = config.dbPort.getOrElse(27017)
+  def database :String = config.database.getOrElse("catalog_manager")
+  def password :String = config.password.getOrElse("")
+  def userName :String = config.userName.getOrElse("")
 }
 

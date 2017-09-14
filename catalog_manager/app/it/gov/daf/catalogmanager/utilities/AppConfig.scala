1739d80422fde17abceeca7e1835af598e740ec3
acherici <a.cherici@gmail.com>
2017-09-14 17:12:39 +0200

    bug fixing

--- catalog_manager/app/it/gov/daf/catalogmanager/utilities/AppConfig.scala
+++ catalog_manager/app/it/gov/daf/catalogmanager/utilities/AppConfig.scala
@@ -20,7 +20,11 @@ private class AppConfig @Inject()(playConfig: Configuration) {
   val password :Option[String] = playConfig.getString("mongo.password")
   val database :Option[String] = playConfig.getString("mongo.database")
   val localUrl :Option[String] = playConfig.getString("app.local.url")
+
   val registrationUrl :Option[String] = playConfig.getString("app.registration.url")
+  val ipaUrl :Option[String] = playConfig.getString("ipa.url")
+  val ipaUser :Option[String] = playConfig.getString("ipa.user")
+  val ipaUserPwd :Option[String] = playConfig.getString("ipa.userpwd")
 }
 
 
@@ -35,6 +39,10 @@ object ConfigReader {
   def password :String = config.password.getOrElse("")
   def userName :String = config.userName.getOrElse("")
   def localUrl :String = config.localUrl.getOrElse("http://localhost:9001")
+
   def registrationUrl :String = config.registrationUrl.getOrElse("http://localhost:3000/confirmregistration?t=")
+  def ipaUrl :String = config.ipaUrl.getOrElse("xxx")
+  def ipaUser :String = config.ipaUser.getOrElse("xxx")
+  def ipaUserPwd :String = config.ipaUserPwd.getOrElse("xxx")
 }
 

a128a5aa360701948d0914966df8dcbf50a8aaf8
acherici <a.cherici@gmail.com>
2017-10-01 15:11:00 +0200

    [DATAPOR-109] [DATAPOR-66] Other SSO stuff

---
+++ catalog_manager/app/it/gov/daf/sso/common/SecuredInvocationManager.scala
@@ -0,0 +1,85 @@
+package it.gov.daf.sso.common
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import org.asynchttpclient.DefaultAsyncHttpClientConfig
+import play.api.libs.json._
+import play.api.libs.ws.WSResponse
+import play.api.libs.ws.ahc.AhcWSClient
+import scala.concurrent.Future
+
+class SecuredInvocationManager(loginClient:LoginClient) {
+
+  import scala.concurrent.ExecutionContext.Implicits._
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  private val sslconfig = new DefaultAsyncHttpClientConfig.Builder().setAcceptAnyCertificate(true).build
+  private val _loginClient=loginClient
+
+
+  private def callService( wsClient:AhcWSClient, loginInfo:LoginInfo, serviceFetch:(String,AhcWSClient)=> Future[WSResponse]):Future[WSResponse] = {
+
+    val cookieOpt = CacheWrapper.getCookie(loginInfo.appName,loginInfo.user)
+
+    if( cookieOpt.isEmpty )
+
+      _loginClient.login(loginInfo, wsClient).flatMap { cookie =>
+
+        CacheWrapper.putCookie(loginInfo.appName,loginInfo.user,cookie)
+
+        serviceFetch(cookie, wsClient).map({ response =>
+          println("RESPONSE:"+response.json)
+          response
+        }).andThen { case _ => wsClient.close() }
+          .andThen { case _ => system.terminate() }
+
+      }
+
+    else
+
+      serviceFetch( cookieOpt.get, wsClient).map{ response =>
+        if(response.status == 200)
+          println("RESPONSE:"+response.json)
+        response
+      }
+
+  }
+
+
+  def manageServiceCall( loginInfo:LoginInfo, serviceFetch:(String,AhcWSClient)=> Future[WSResponse] ) : Future[JsValue] = {
+
+    val wsClient = AhcWSClient(sslconfig)
+
+
+    callService(wsClient,loginInfo,serviceFetch) flatMap {response =>
+
+      if(response.status == 401){
+        println("Unauthorized!!")
+        CacheWrapper.deleteCookie(loginInfo.appName,loginInfo.user)
+        callService(wsClient,loginInfo,serviceFetch).map(_.json)
+          .andThen { case _ => wsClient.close() }
+          .andThen { case _ => system.terminate() }
+      }else
+         Future{ response.json }
+
+    }
+
+  }
+
+}
+
+
+object SecuredInvocationManager{
+
+  private var _instance : SecuredInvocationManager = null
+
+  def instance(loginClient:LoginClient) = {
+    if (_instance == null)
+      _instance = new SecuredInvocationManager(loginClient:LoginClient)
+    _instance
+  }
+
+}
+
+

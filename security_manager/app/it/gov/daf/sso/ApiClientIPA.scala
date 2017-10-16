a25561de1cf67b0bd4199fc5e727ffaa032ce4a0
acherici <a.cherici@gmail.com>
2017-10-16 19:49:11 +0200

    [DATPOR-91] - Login with email

--- security_manager/app/it/gov/daf/sso/ApiClientIPA.scala
+++ security_manager/app/it/gov/daf/sso/ApiClientIPA.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import it.gov.daf.securitymanager.service.utilities.{ConfigReader, WebServiceUtil}
 import it.gov.daf.sso.common.{LoginInfo, SecuredInvocationManager}
+import org.apache.commons.lang3.StringEscapeUtils
 import play.api.libs.json._
 import play.api.libs.ws.WSResponse
 import play.api.libs.ws.ahc.AhcWSClient
@@ -23,93 +24,6 @@ object ApiClientIPA {
   private val secInvokeManager = SecuredInvocationManager.instance(loginClient)
 
 
-  //private val CKAN_URL = ConfigReader.getCkanHost
-  //private val IPA_URL = ConfigReader.ipaUrl
-  //private val IPA_APP_ULR = IPA_URL+"/ipa"
-  //private val IPA_SERVICES_URL = IPA_URL+"/ipa/session/json"
-  //private val IPA_LOGIN_ULR = IPA_URL+"/ipa/session/login_password"
-  //private val USER_PASSWORD_POST_DATA = s"user=${ConfigReader.ipaUser}&password=${ConfigReader.ipaUserPwd}"
-  //private val sslconfig = new DefaultAsyncHttpClientConfig.Builder().setAcceptAnyCertificate(true).build
-
-  //private var sessionCookie:String=null
-
-
-
-/*
-  private def login( wsClient:AhcWSClient ):Future[String] = {
-
-    val wsResponse = wsClient.url(IPA_LOGIN_ULR).withHeaders(  "Content-Type"->"application/x-www-form-urlencoded",
-                                              "Accept"->"text/plain",
-                                              "referer"->IPA_APP_ULR
-    ).post(USER_PASSWORD_POST_DATA)
-
-    println("login")
-
-    wsResponse map { response =>
-
-      val setCookie=response.header("Set-Cookie").getOrElse( throw new Exception("Set-Cookie header not found") )
-      println("SET COOKIE: "+setCookie)
-      val cookie = setCookie.split(";")(0)
-      println("COOKIE: "+cookie)
-      cookie
-
-    }
-
-  }
-*/
-
-/*
-
-  private def callIpaService( wsClient:AhcWSClient,jsIn:JsValue, serviceCall:(AhcWSClient,JsValue)  => Future[WSResponse]):Future[WSResponse] = {
-
-
-    if( sessionCookie == null )
-
-      login(wsClient).flatMap { cookie =>
-
-        sessionCookie = cookie
-
-        serviceCall(wsClient, jsIn ).map({ response =>
-          println("RESPONSE:"+response.json)
-          response
-        }).andThen { case _ => wsClient.close() }
-          .andThen { case _ => system.terminate() }
-
-      }
-
-    else
-
-      serviceCall(wsClient, jsIn).map{ response =>
-        if(response.status == 200)
-          println("RESPONSE:"+response.json)
-        response
-      }
-
-
-  }
-
-
-
-  private def manageServiceCall(payload:JsValue, serviceCall:(AhcWSClient,JsValue)  => Future[WSResponse]) : Future[JsValue] = {
-
-    val wsClient = AhcWSClient(sslconfig)
-
-    callIpaService(wsClient,payload,serviceCall) flatMap {response1 =>
-
-      if(response1.status == 401){
-        println("Unauthorized!!")
-        sessionCookie=null
-        callIpaService(wsClient,payload,serviceCall).map({response2 => response2.json})
-          .andThen { case _ => wsClient.close() }
-          .andThen { case _ => system.terminate() }
-      }else
-         Future{ response1.json }
-
-    }
-
-  }*/
-
-
   def createUser(user: IpaUser):Future[Either[Error,Success]]= {
 
 
@@ -126,7 +40,7 @@ object ApiClientIPA {
                                              "givenname":"${user.givenname}",
                                              "sn":"${user.sn}",
                                              "mail":"${user.mail}",
-                                             "userpassword":"${user.userpassword.get}",
+                                             "userpassword":"${StringEscapeUtils.escapeJson(user.userpassword.get)}",
 
                                              "no_members":false,
                                              "noprivate":false,
@@ -197,6 +111,48 @@ object ApiClientIPA {
 
   }
 
+  def findUserByMail(mail: String):Future[Either[Error,IpaUser]]={
+
+    val jsonRequest:JsValue = Json.parse(s"""{
+                                             "id": 0,
+                                             "method": "user_find",
+                                             "params": [
+                                                 [""],
+                                                 {
+                                                    "mail": "$mail",
+                                                    "version": "2.213"
+                                                 }
+                                             ]
+                                         }""")
+
+    //println(jsonRequest.toString())
+
+    val serviceInvoke : (String,AhcWSClient)=> Future[WSResponse] = callIpaUrl(jsonRequest,_,_)
+    secInvokeManager.manageServiceCall(loginInfo,serviceInvoke).map { json =>
+
+      val count = ((json \ "result") \ "count").asOpt[Int].getOrElse(-1)
+      val result = ((json \ "result") \"result")(0)//.getOrElse(JsString("null")).toString()
+
+      if(count==0)
+        Left( Error(Option(0),Some("No user found"),None) )
+
+      else if( result == "null" || result.isInstanceOf[JsUndefined]  )
+        Left( Error(Option(0),Some(readIpaErrorMessage(json)),None) )
+
+      else
+        Right(
+          IpaUser(
+            (result \ "sn") (0).asOpt[String].getOrElse(""),
+            (result \ "givenname") (0).asOpt[String].getOrElse(""),
+            (result \ "mail") (0).asOpt[String].getOrElse(""),
+            (result \ "uid") (0).asOpt[String].getOrElse(""),
+            None
+          )
+        )
+
+    }
+
+  }
 
   private def callIpaUrl( payload: JsValue, sessionCookie:String, cli:AhcWSClient ): Future[WSResponse] = {
 
@@ -229,6 +185,28 @@ object ApiClientIPA {
       .andThen { case _ => system.terminate() }
 
   }
+
+  /*
+  private def bindDefaultOrg(userName:String):Future[String] = {
+
+    val wsClient = AhcWSClient()
+
+    println("bind default organization")
+
+    wsClient.url()
+
+    wsResponse.map({ response =>
+
+      if( response != null  )
+        "ok"
+      else
+        throw new Exception("Failed to login to ckan")
+
+
+    }).andThen { case _ => wsClient.close() }
+      .andThen { case _ => system.terminate() }
+
+  }*/
 
   private def readIpaErrorMessage( json:JsValue )={
 

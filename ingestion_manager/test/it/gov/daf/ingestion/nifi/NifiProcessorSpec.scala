55675d7de9580af11e3067a7acc7681638079bf4
fabio fumarola <fabiofumarola@gmail.com>
2017-11-07 18:21:51 +0100

    test completed

--- ingestion_manager/test/it/gov/daf/ingestion/nifi/NifiProcessorSpec.scala
+++ ingestion_manager/test/it/gov/daf/ingestion/nifi/NifiProcessorSpec.scala
@@ -1,11 +1,17 @@
 package it.gov.daf.ingestion.nifi
 
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.typesafe.config.Config
 import it.gov.daf.catalogmanager.json._
 import it.gov.daf.catalogmanager.{Dataset, MetaCatalog, Operational}
 import org.scalatest.{FlatSpec, Matchers}
 import play.api.libs.json._
+import play.api.libs.ws.ahc.AhcWSClient
 
 import scala.io.Source
+import scala.util.{Failure, Success}
 
 class NifiProcessorSpec extends FlatSpec with Matchers {
 
@@ -19,9 +25,21 @@ class NifiProcessorSpec extends FlatSpec with Matchers {
     println(parsed)
     val metaCatalog: JsResult[MetaCatalog] = Json.fromJson[MetaCatalog](parsed)
 
-    println(metaCatalog)
+    metaCatalog.isSuccess shouldBe true
 
+    implicit val config: Config = com.typesafe.config.ConfigFactory.load()
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val wsClient: AhcWSClient = AhcWSClient()
+    implicit val ec = system.dispatcher
 
+    val fResult = NifiProcessor(metaCatalog.get).createDataFlow()
 
+    fResult.onComplete {
+      case Success(response) =>
+        true shouldBe true
+
+      case Failure(ex) =>
+    }
   }
 }

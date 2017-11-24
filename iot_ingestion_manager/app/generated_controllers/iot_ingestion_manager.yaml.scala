67b402a1a8628a0e1abe5f0c08c6655eb1aae7de
fabiana <fabiana.lan@hotmail.it>
2017-11-24 16:20:04 +0100

    add SerializerDeserializerSpec

--- iot_ingestion_manager/app/generated_controllers/iot_ingestion_manager.yaml.scala
+++ iot_ingestion_manager/app/generated_controllers/iot_ingestion_manager.yaml.scala
@@ -45,54 +45,9 @@ import scala.annotation.tailrec
 import scala.collection.convert.decorateAsJava._
 import scala.language.postfixOps
 import scala.util.{Failure,Success,Try}
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.spark.rdd.RDD
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
 import org.apache.spark.opentsdb.DataPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
-import org.apache.kudu.ColumnSchema
-import org.apache.kudu.client.CreateTableOptions
 import org.apache.kudu.ColumnSchema
 import org.apache.kudu.client.CreateTableOptions
 import org.apache.kudu.ColumnSchema
@@ -105,7 +60,7 @@ import org.apache.kudu.client.CreateTableOptions
 
 package iot_ingestion_manager.yaml {
     // ----- Start of unmanaged code area for package Iot_ingestion_managerYaml
-                                
+                                    
   @SuppressWarnings(
     Array(
       "org.wartremover.warts.While",

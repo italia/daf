97173c6bd384b2c158287670bda1587db285f246
fabio fumarola <fabiofumarola@gmail.com>
2017-12-06 13:57:26 +0100

    add call to init sftp folder in the ingestion manager

--- ingestion_manager/app/generated_controllers/ingestion_manager.yaml.scala
+++ ingestion_manager/app/generated_controllers/ingestion_manager.yaml.scala
@@ -1,34 +1,18 @@
 
-import play.api.mvc.{Action,Controller}
-
-import play.api.data.validation.Constraint
-
-import play.api.i18n.MessagesApi
-
-import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
-
-import de.zalando.play.controllers._
-
-import PlayBodyParsing._
-
-import PlayValidations._
-
-import scala.util._
-
 import javax.inject._
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import com.typesafe.config.Config
 import de.zalando.play.controllers.PlayBodyParsing._
 import it.gov.daf.ingestion.ClientCaller
-import play.api.libs.ws.ahc.AhcWSClient
-import scala.concurrent.Future
-import com.typesafe.config.Config
 import it.gov.daf.ingestion.nifi.NifiProcessor
-import it.gov.daf.ingestion.nifi.NifiProcessor.NifiResult
-import play.api.libs.ws.WSClient
-import scala.concurrent.ExecutionContext
 import it.gov.daf.ingestion.pipelines.PipelineInfoRead
+import play.api.i18n.MessagesApi
+import play.api.inject.{ApplicationLifecycle, ConfigurationProvider}
+import play.api.libs.ws.WSClient
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller is re-generated after each change in the specification.
@@ -36,29 +20,35 @@ import it.gov.daf.ingestion.pipelines.PipelineInfoRead
  */
 
 package ingestion_manager.yaml {
-    // ----- Start of unmanaged code area for package Ingestion_managerYaml
-                
-    // ----- End of unmanaged code area for package Ingestion_managerYaml
-    class Ingestion_managerYaml @Inject() (
-        // ----- Start of unmanaged code area for injections Ingestion_managerYaml
+
+  import it.gov.daf.securitymanager.client.Security_managerClient
+  // ----- Start of unmanaged code area for package Ingestion_managerYaml
+
+  // ----- End of unmanaged code area for package Ingestion_managerYaml
+  class Ingestion_managerYaml @Inject() (
+    // ----- Start of unmanaged code area for injections Ingestion_managerYaml
     implicit
     val system: ActorSystem,
     implicit val ec: ExecutionContext,
     implicit val ws: WSClient,
-        // ----- End of unmanaged code area for injections Ingestion_managerYaml
-        val messagesApi: MessagesApi,
-        lifecycle: ApplicationLifecycle,
-        config: ConfigurationProvider
-    ) extends Ingestion_managerYamlBase {
-        // ----- Start of unmanaged code area for constructor Ingestion_managerYaml
+    // ----- End of unmanaged code area for injections Ingestion_managerYaml
+    val messagesApi: MessagesApi,
+    lifecycle: ApplicationLifecycle,
+    config: ConfigurationProvider
+  ) extends Ingestion_managerYamlBase {
+    // ----- Start of unmanaged code area for constructor Ingestion_managerYaml
     NotImplementedYet
-        // ----- End of unmanaged code area for constructor Ingestion_managerYaml
-        val addNewDataset = addNewDatasetAction { (ds_logical_uri: String) =>  
-            // ----- Start of unmanaged code area for action  Ingestion_managerYaml.addNewDataset
-            //FIXME take out these resources and close them
+    // ----- End of unmanaged code area for constructor Ingestion_managerYaml
+    val addNewDataset = addNewDatasetAction { (ds_logical_uri: String) =>
+      // ----- Start of unmanaged code area for action  Ingestion_managerYaml.addNewDataset
+      //FIXME take out these resources and close them
       implicit val config: Config = com.typesafe.config.ConfigFactory.load()
       implicit val materializer: ActorMaterializer = ActorMaterializer()
       val clientCaller = new ClientCaller(ws)
+
+      val securityClient = new Security_managerClient(ws)(
+        baseUrl = config.getString("daf.security_manager_url")
+      )
 
 
       val auth = currentRequest
@@ -68,7 +58,17 @@ package ingestion_manager.yaml {
 
       val fResults =
         clientCaller.clientCatalogMgrMetaCatalog(auth, ds_logical_uri)
-          .flatMap(mc => NifiProcessor(mc).createDataFlow())
+        .flatMap{ mc =>
+          //TODO move this logic into common
+          val user = mc.operational.group_own
+          val domain = mc.operational.theme
+          val subDomain = mc.operational.subtheme
+          val dsName = mc.dcatapit.name
+          val path = s"/home/$user/$domain/$subDomain/$dsName"
+          securityClient.sftp(auth,mc.operational.group_own, path)
+            .map(res => mc)
+        }
+        .flatMap(mc => NifiProcessor(mc).createDataFlow())
 
       fResults
         .flatMap(r =>
@@ -78,23 +78,23 @@ package ingestion_manager.yaml {
             ex.printStackTrace()
             AddNewDataset200(IngestionReport("400", Some(ex.getLocalizedMessage)))
         }
-            // ----- End of unmanaged code area for action  Ingestion_managerYaml.addNewDataset
-        }
-        val pipelineListAll = pipelineListAllAction {  _ =>  
-            // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListAll
-            PipelineListAll200(PipelineInfoRead.pipelineInfo())
-            // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListAll
-        }
-        val pipelineListByCat = pipelineListByCatAction { (pipeline_category: String) =>  
-            // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListByCat
-            PipelineListByCat200(PipelineInfoRead.pipelineInfoByCat(pipeline_category))
-            // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListByCat
-        }
-        val pipelineListById = pipelineListByIdAction { (pipeline_id: String) =>  
-            // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListById
-            PipelineListById200(PipelineInfoRead.pipelineInfoById(pipeline_id))
-            // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListById
-        }
-    
+      // ----- End of unmanaged code area for action  Ingestion_managerYaml.addNewDataset
     }
+    val pipelineListAll = pipelineListAllAction { _ =>
+      // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListAll
+      PipelineListAll200(PipelineInfoRead.pipelineInfo())
+      // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListAll
+    }
+    val pipelineListByCat = pipelineListByCatAction { (pipeline_category: String) =>
+      // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListByCat
+      PipelineListByCat200(PipelineInfoRead.pipelineInfoByCat(pipeline_category))
+      // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListByCat
+    }
+    val pipelineListById = pipelineListByIdAction { (pipeline_id: String) =>
+      // ----- Start of unmanaged code area for action  Ingestion_managerYaml.pipelineListById
+      PipelineListById200(PipelineInfoRead.pipelineInfoById(pipeline_id))
+      // ----- End of unmanaged code area for action  Ingestion_managerYaml.pipelineListById
+    }
+
+  }
 }

249747827be2f3e239262025b5302e5fd4212989
fabio fumarola <fabiofumarola@gmail.com>
2017-12-21 13:59:45 +0100

    Feature/infrastructure (#183)
    * add infrastructure doc
    
    * add glusterfs
    
    * Nginx ingress controller deployment steps link added
    
    * completed glusterfs
    
    * Update glusterfs.md
    
    * last modifies to glusterfs doc
    
    * add livy conf
    
    * add jupyter hub
    
    * now livy works
    
    * update jupyter doc
    
    * completed gluster with secrets
    
    * Adapted Ingress for kubernetes 1.9
    
    Added documentation
    
    * added conf livy
    
    * add ingress rule to access the old superset version

---
+++ infrastructure/kong/add_apis.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-storage-manager' \
+  --data 'uris=/storage-manager' \
+  --data 'upstream_url=http://storage-manager.default.svc.cluster.local:9000/storage-manager'
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-security-manager' \
+  --data 'uris=/security-manager' \
+  --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/security-manager'
+
+ curl -i -X POST \
+ --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+ --data 'name=daf-sso-manager' \
+ --data 'uris=/sso-manager' \
+ --data 'upstream_url=http://security-manager.default.svc.cluster.local:9000/sso-manager'
+
+ curl -i -X POST \
+ --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+ --data 'name=daf-catalog-manager' \
+ --data 'uris=/catalog-manager' \
+ --data 'upstream_url=http://catalog-manager.default.svc.cluster.local:9000/catalog-manager'
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-datipubblici' \
+  --data 'uris=/dati-gov' \
+  --data 'upstream_url=http://datipubblici.default.svc.cluster.local:9000/dati-gov'
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-iot-ingestion-manager' \
+  --data 'uris=/iot-ingestion-manager' \
+  --data 'upstream_url=http://iot-ingestion-manager.default.svc.cluster.local:9900/iot-ingestion-manager'
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-opentsdb' \
+  --data 'uris=/opentsdb/v1' \
+  --data 'upstream_url=http://opentsdb.default.svc.cluster.local:4242'
+
+curl -i -X POST \
+  --url http://kong-kong.default.svc.cluster.local:8001/apis/ \
+  --data 'name=daf-livy' \
+  --data 'uris=/livy' \
+  --data 'upstream_url=http://livy.default.svc.cluster.local:8998'
---
+++ infrastructure/setup/kubernetes.md
@@ -0,0 +1,6 @@
+# Kubernetes Setup
+
+This was done by using `kubeadm` and following steps [in](https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/):
+
+1. [install docker kubeadm and kubelet](https://kubernetes.io/docs/setup/independent/install-kubeadm/#before-you-begin)
+2. [create the cluster](https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/)

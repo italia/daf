c26428b9edf2115f487bf0a5ee15921636a677d2
fabio fumarola <fabiofumarola@gmail.com>
2017-12-27 16:04:42 +0100

    minor

--- storage_manager/playApi/app/daf/dataset/DatasetService.scala
+++ storage_manager/playApi/app/daf/dataset/DatasetService.scala
@@ -101,7 +101,7 @@ class DatasetService(
           Try(
             Map(
               "protocol" -> "hdfs",
-              "path" -> catalog.operational.physical_uri.get //storage.hdfs.flatMap(_.path).map(_ + "/final.parquet").get
+              "path" -> s"${catalog.operational.physical_uri.get}/final.parquet" //storage.hdfs.flatMap(_.path).map(_ + "/final.parquet").get
             )
           )
         } else if (storage.kudu.isDefined) {

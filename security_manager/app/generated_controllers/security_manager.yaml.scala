74ff33a6488ecad5da909acc945b2a0326cb16c1
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-02-05 17:16:39 +0100

    Minor

--- security_manager/app/generated_controllers/security_manager.yaml.scala
+++ security_manager/app/generated_controllers/security_manager.yaml.scala
@@ -38,7 +38,7 @@ import it.gov.daf.ftp.SftpHandler
 
 package security_manager.yaml {
     // ----- Start of unmanaged code area for package Security_managerYaml
-                                                                                                                                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                                                                                                                    
     // ----- End of unmanaged code area for package Security_managerYaml
     class Security_managerYaml @Inject() (
         // ----- Start of unmanaged code area for injections Security_managerYaml

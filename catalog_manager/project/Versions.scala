5d028c45e205555a8ca8365033e672f9aa91c76d
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-04-18 23:09:05 +0200

    ckan embedded

--- catalog_manager/project/Versions.scala
+++ catalog_manager/project/Versions.scala
@@ -4,5 +4,5 @@ object Versions {
 
   val swaggerUiVersion = "3.0.10"
 
-  val dafCommonVersion = sys.env.get("COMMON_VERSION").getOrElse("1.0.6-SNAPSHOT")
+  val dafCommonVersion = sys.env.get("COMMON_VERSION").getOrElse("1.0.7-SNAPSHOT")
 }

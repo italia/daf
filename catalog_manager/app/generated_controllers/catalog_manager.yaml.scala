dba2900a6b68cedd0f87afc67e28beaedce4738a
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-07-11 14:56:37 +0200

    added 20 sec await for sftp

--- catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
+++ catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
@@ -49,7 +49,7 @@ import scala.concurrent.Await
 
 package catalog_manager.yaml {
     // ----- Start of unmanaged code area for package Catalog_managerYaml
-                            
+                                
     // ----- End of unmanaged code area for package Catalog_managerYaml
     class Catalog_managerYaml @Inject() (
         // ----- Start of unmanaged code area for injections Catalog_managerYaml
@@ -550,7 +550,7 @@ package catalog_manager.yaml {
               .withAuth(KYLOUSER, KYLOPWD, WSAuthScheme.BASIC)
 
             // it is a try i know is not a good practice
-            Await.result(createDir.get(), 50000 millis)
+            Await.result(createDir.get(), 200000 millis)
             //Await.ready(createDir.get, 50000 millis)
 
             val feedData: Future[JsResult[JsObject]] = for {

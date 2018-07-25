96e8711ae0251b510dce881d906372f2c5eecee0
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-07-25 11:16:19 +0200

    Try enhance timeout

--- catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
+++ catalog_manager/app/generated_controllers/catalog_manager.yaml.scala
@@ -50,7 +50,7 @@ import yaml.ResponseWrites.MetaCatalogWrites.writes
 
 package catalog_manager.yaml {
     // ----- Start of unmanaged code area for package Catalog_managerYaml
-                        
+                            
     // ----- End of unmanaged code area for package Catalog_managerYaml
     class Catalog_managerYaml @Inject() (
         // ----- Start of unmanaged code area for injections Catalog_managerYaml

cf6374b4e2ebb068af126d534c5624b605aa1025
Glyton Camilleri <g.camilleri@luzzu-lab.com>
2018-07-26 17:59:39 +0200

    Added a test to produce message on Kafka

---
+++ iot_ingestion_manager/test/daf/stream/avro/EventProducerSpec.scala
@@ -0,0 +1,48 @@
+package daf.stream.avro
+
+import java.util.concurrent.TimeUnit
+
+import config.KafkaConfig
+import it.gov.daf.iot.event.Event
+import org.apache.kafka.clients.producer.{ KafkaProducer, ProducerRecord }
+import org.apache.kafka.common.serialization.{ ByteArraySerializer, StringSerializer }
+import org.scalacheck.Gen
+import org.scalacheck.rng.Seed
+import org.scalatest.{ MustMatchers, WordSpec }
+
+import scala.concurrent.duration._
+
+class EventProducerSpec extends WordSpec with MustMatchers {
+
+  private type Bytes = Array[Byte]
+
+  private val kafkaConfig = KafkaConfig(
+//    servers      = Seq("edge1.novalocal:9092", "edge1.novalocal:9092"),
+    servers      = Seq.empty,
+    groupId      = "test-group-1",
+    timeout      = 10.seconds,
+    numProducers = 1,
+    topicConfig = null
+  )
+
+  private val producer = new KafkaProducer[String, Bytes](kafkaConfig.producerProps(), new StringSerializer, new ByteArraySerializer)
+
+  private def sample(numEvents: Int) = Gen.listOfN(numEvents, AvroGen.event).pureApply(Gen.Parameters.default, Seed.random())
+
+  private def createRecord(topic: String, event: Event) = new ProducerRecord[String, Bytes](
+    topic,
+    event.getId.toString,
+    EventSerDe.serialize(event)
+  )
+
+  private def send(numEvents: Int, topic: String) = sample(numEvents).foreach { event =>
+    producer.send { createRecord(topic, event) }.get(kafkaConfig.timeout.toMillis, TimeUnit.MILLISECONDS)
+  }
+
+  "Event Producer" must {
+
+    "send events" in send(5, "iot-topic-1")
+
+  }
+
+}

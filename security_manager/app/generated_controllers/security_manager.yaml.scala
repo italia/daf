ada55979f8d89d9641a3f686d43176708098936e
acherici <a.cherici@gmail.com>
2018-08-07 17:40:33 +0200

    Improvements on open data management

--- security_manager/app/generated_controllers/security_manager.yaml.scala
+++ security_manager/app/generated_controllers/security_manager.yaml.scala
@@ -46,7 +46,7 @@ import scala.collection.immutable.StringLike
 
 package security_manager.yaml {
     // ----- Start of unmanaged code area for package Security_managerYaml
-    
+                    
     // ----- End of unmanaged code area for package Security_managerYaml
     class Security_managerYaml @Inject() (
         // ----- Start of unmanaged code area for injections Security_managerYaml

1931ed142d928b8a28f0612dcec5f4771799ab9a
gruggiero <giovanni.ruggiero@gmail.com>
2018-09-25 15:35:09 +0200

    Updated storage_manager version

--- storage_manager/project/Versions.scala
+++ storage_manager/project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
 
-  lazy val defaultVersion = sys.env.getOrElse("STORAGE_MANAGER_VERSION", "1.0.2")
+  lazy val defaultVersion = sys.env.getOrElse("STORAGE_MANAGER_VERSION", "1.0.3-SNAPSHOT")
 
   lazy val isSnapshot = Versions.defaultVersion.endsWith("SNAPSHOT")
 
